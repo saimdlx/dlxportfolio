ba76deb0b6604987dd8bddcbb036dc2b2a17f752
saimdlx <1629saim@gmail.com>
2026-03-15 23:14:05 -0700

    (commit) reworked website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,49 @@
 'use client'
-import AnimateLinks from "../components/linksanimation";
-import PixelBlast from '../components/bitbackground';
+
+import './properties.css';
+
 
 export default function Home() {
 
   return (
-    <div className="font-mono grid grid-rows-[20px_1fr_20px] justify-items-center items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <PixelBlast
-        style={{ width: '100%', height: '100%', position: 'absolute', padding: 0, margin: 0, top: 0, left: 0, zIndex: 1 }}
-        variant="square"
-        pixelSize={1}
-        color="orchid"
-        patternScale={2}
-        patternDensity={0.5}
-        pixelSizeJitter={0.5}
-        rippleSpeed={0.4}
-        rippleThickness={0.12}
-        rippleIntensityScale={1.5}
-        speed={1}
-        edgeFade={0.10}
-      />
-      <main className="flex flex-col gap-[32px] row-start-2 place-items-center-safe sm:items-center-safe z-2">
+    <div className="root">
+      <div className="home-wrapper">
+        <div className="home-container">
+          <div className="sidebar">
 
-        <div className="text-xl font-bold">
-          <AnimateLinks className="text-decoration-line: underline" setColor="orchid" href="https://github.com/saimdlx">GitHub</AnimateLinks> - <AnimateLinks className="text-decoration-line: underline" setColor="orchid" href="https://devpost.com/saimdlx?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav">DevPost</AnimateLinks> - <AnimateLinks className="text-decoration-line: underline" setColor="orchid" href="https://www.linkedin.com/in/ssaim/">LinkedIn</AnimateLinks> - <AnimateLinks className="text-decoration-line: underline" setColor="orchid" href="">Blog</AnimateLinks>
+            <header>
+              <h1>
+                <a href="/">saim's</a>
+              </h1>
+            </header>
+
+            <nav>
+              <ul>
+                <li>
+                  <a className="" href="/">Home</a>
+                </li>
+                <li>
+                  <a className="" href="">About</a>
+                </li>
+                <li>
+                  <a className="" href="">Projects</a>
+                </li>
+                <li>
+                  <a className="" href="">Contact</a>
+                </li>
+                <li>
+                  <a className="" href="">Blog</a>
+                </li>
+
+              </ul>
+            </nav>
+          </div>
+
+          <main>
+            <img src="/waves.gif"/>
+          </main>
         </div>
-
-
-        <div className="tracking-widest text-4xl font-bold">
-          hey, im saim!
-        </div>
-
-        <div className="text-xl font-bold text-wrap">
-          im a computer science student and aspiring software engineer.<br></br>
-          i currently work for <AnimateLinks className="text-decoration-line: underline" setColor="orchid" href="https://toolhouse.ai/">ToolHouse</AnimateLinks> as an ambassador!
-        </div>
-
-      </main>
-
+      </div>
     </div>
   );
 }
